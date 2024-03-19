4080ab23e2666e7e786807ea5e80700d27d3c387
HugoLundblad <hugo01.lundblad@gmail.com>
2024-03-19 13:24:39 +0100

    Removed vertical slider

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SpawnItems()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
