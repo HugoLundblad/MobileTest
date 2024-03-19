0282d89b59ca9be6624ba96eb44c9894c8862445
HugoLundblad <hugo01.lundblad@gmail.com>
2024-03-19 15:58:12 +0100

    Started work on initialization

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public Color[] color;
     public GameObject item;
     // Start is called before the first frame update
     void Start()
